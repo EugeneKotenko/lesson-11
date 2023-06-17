3c09f420c19c3fbee42230d870f6dc61ac0504cd
EugeneKotenko <134332591+EugeneKotenko@users.noreply.github.com>
2023-06-17 18:53:45 +0300

    lesson-11

---
+++ index.js
@@ -0,0 +1,14 @@
+"use strict"
+
+function pow(num, degree) {
+    if (degree === 0) {
+        return 1;
+    } else if (degree < 0) {
+        return 1 / pow (num, -degree)
+    } else {
+        return num * pow (num, degree - 1);
+    }
+}
+
+console.log (pow(2,2)) // enter any number
+
